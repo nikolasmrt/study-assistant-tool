daceb0a729ac24ada42c00874e6ed2ff66fd0c6b
Nikolas Martins <nikoti2024@gmail.com>
2025-12-02 06:39:33 -0300

    update

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000', // URL do seu Backend FastAPI
+
+    baseURL: 'https://study-assistant-api.onrender.com', // <--- SUA URL DO RENDER AQUI
 });
 
 export default api;
